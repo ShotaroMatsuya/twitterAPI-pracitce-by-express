cd954ea38a4d6c2049139a840a1d0273afa3e9e3
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-10-27 01:49:37 +0900

    implement api endpoint

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const Twit = require('twit');
 //init twit
 
 const T = new Twit({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
-  access_token_secret: '',
+  consumer_key: 'HodZaffQVo0jQWVqCLQRvZGGP',
+  consumer_secret: 'bgCbTIucmZStzwVSJL5BWuzyPIdiiYHlZDIupPtErHNmCRs6sQ',
+  access_token: '1312553589965942784-S4srJ3diuMe941bBT2lec9OU0pnlTY',
+  access_token_secret: '8rOAYH4SE0dGzoXor9zLwOLbBrEyaFuOvdEb6DJtoutzs',
   timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
   strictSSL: true, // optional - requires SSL certificates to be valid.
 });
@@ -18,12 +18,21 @@ const T = new Twit({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extend: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.get('/tweets/:search', (req, res) => {
   T.get(
     'search/tweets',
-    { q: req.params.search, count: 5 },
+    { q: req.params.search, count: 1 },
     (err, data, response) => {
       res.json(data);
     }
@@ -41,11 +50,11 @@ app.post('/comment', (req, res) => {
     }
   );
 });
-app.get('/comment/chelsea', (req, res) => {
-  const screen_name = 'ChelseaFC';
+app.get('/timeline/shotaro', (req, res) => {
+  const screen_name = 'Shotaro59432703';
   T.get(
     'statuses/user_timeline',
-    { screen_name: screen_name, count: 4 },
+    { screen_name: screen_name, count: 8 },
     function (err, data, response) {
       res.json(data);
     }
