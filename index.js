15602ebe95b8d18faa3f1759edd44e96c6616e9f
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-10-29 08:29:51 +0900

    prepared for deployment

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const bodyParser = require('body-parser');
 const Twit = require('twit');
+const helmet = require('helmet');
+const https = require('https');
+
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
 
 //init twit
 
 const T = new Twit({
-  consumer_key: 'HodZaffQVo0jQWVqCLQRvZGGP',
-  consumer_secret: 'bgCbTIucmZStzwVSJL5BWuzyPIdiiYHlZDIupPtErHNmCRs6sQ',
-  access_token: '1312553589965942784-S4srJ3diuMe941bBT2lec9OU0pnlTY',
-  access_token_secret: '8rOAYH4SE0dGzoXor9zLwOLbBrEyaFuOvdEb6DJtoutzs',
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
   strictSSL: true, // optional - requires SSL certificates to be valid.
 });
+app.use(helmet());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -57,9 +64,11 @@ app.get('/timeline/shotaro', (req, res) => {
     { screen_name: screen_name, count: 6 },
     function (err, data, response) {
       res.json(data);
+      console.log("success");
     }
   );
 });
 
-app.listen(3000);
+// https.createServer({key:privateKey,cert:certificate},app).listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
 console.log('**RESTART**');
